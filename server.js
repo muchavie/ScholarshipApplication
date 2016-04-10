fa540896304cd09d3596a142805b5ba92d841dc3
Peter Ogilvie <code@ogilvie.us.com>
2016-04-10 15:55:50 -0700

    use twitter bootstrap for style

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var db = require('./db');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var nunjucks = require('nunjucks');
+let express = require('express'),
+   passport = require('passport'),
+   Strategy = require('passport-local').Strategy,
+         db = require('./db'),
+         fs = require('fs'),
+       http = require('http'),
+      https = require('https'),
+   nunjucks = require('nunjucks');
 
 
+// Credentials are only needed in the production https environment
 var privateKey, certificate, credentials;
 if (process.argv[2] == 'local') {
-    privateKey = null;
+    privateKey  = null;
     certificate = null;
     credentials = null;
 } else {
@@ -21,75 +22,100 @@ if (process.argv[2] == 'local') {
     credentials = {key: privateKey, cert: certificate};
 }
 
-
-
+// The server we're building and the TCP port it will listen on
+var server;
+let port = 4002;
 
 // Configure the local strategy for use by Passport.
+// input: credencials (username, passord), callback (cb)
 //
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+// The verify function (db.users.findByUsername) verified that
+// the given password is correct for the provided password.
+// and invokes the provied callback with success or failure.
+//
+// User object is set to req.user after authentication.
+passport.use(new Strategy(function(username, password, cb) {
+
+        db.users.findByUsername(username, function(err, user) {
+
+            if (err)
+                return cb(err);
+
+            if (!user)
+                return cb(null, false);
+
+            if (user.password != password)
+                return cb(null, false);
+
+            return cb(null, user);
+        });
+}));
 
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
+/*
+ *             PASSPORT AUTHENTICATED SESSION PERSISTENCE.
+ */
 passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
+    cb(null, user.id);
 });
 
 passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
+
+    db.users.findById(id, function (err, user) {
+
+        if (err)
+            return cb(err);
+
+        cb(null, user);
+    });
 });
 
 
-// Create a new Express application.
+/*
+ *             E X P R E S S
+ */
 var app = express();
-// using nunjucks as the template engine
+
+/*
+ *             M I D D L E W A R E
+ */
+
+// Template Engine
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+// Logging
 app.use(require('morgan')('combined'));
+
+// Cookies
 app.use(require('cookie-parser')());
+
+// http request parsing
 app.use(require('body-parser').urlencoded({ extended: true }));
+
+// http session handling
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+// Authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home.njk', { user: req.user });
-  });
+// Static Files
+app.use(express.static('lib'));    // CSS and JS Libraris
 
-app.get('/login',
-  function(req, res){
+/*
+ *            A P P L I C A T I O N  R O U T I N G
+ */
+
+app.get('/',  function(req, res) {
+    res.render('home.njk', { user: req.user });
+});
+
+app.get('/login', function(req, res){
     res.render('login.njk');
-  });
+});
 
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login' }),
@@ -97,21 +123,18 @@ app.post('/login',
     res.redirect('/');
   });
 
-app.get('/logout',
-  function(req, res){
+app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
-  });
+ });
 
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
+app.get('/profile', require('connect-ensure-login').ensureLoggedIn(),
   function(req, res){
     res.render('profile.njk', { user: req.user });
-  });
+});
 
 
-var server;
-let port = 4002;
+
 if (process.argv[2] === 'local') {
     server = http.createServer(app);
     console.log(`Running http on port: ${port}`);
